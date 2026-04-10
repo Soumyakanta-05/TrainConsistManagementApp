23dd9f3d61318ed86301a5ad62745be367c185d4
Soumyakanta-05 <dattaguptasoumyakanta@gmail.com>
2026-04-10 15:10:07 +0530

    UC8: Filter Passenger Bogies Using Streams

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Bogie {
+    String type;
+    int capacity;
+
+    public Bogie(String type, int capacity) {
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public String toString() {
+        return type + " (" + capacity + ")";
+    }
+}
+
+public class Main {
+
+    public static List<Bogie> filterBogies(List<Bogie> bogies, int threshold) {
+        return bogies.stream()
+                .filter(b -> b.getCapacity() > threshold)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 40));
+        bogies.add(new Bogie("Sleeper", 80));
+
+        int threshold = 70;
+
+        List<Bogie> filtered = filterBogies(bogies, threshold);
+
+        System.out.println("Filtered Bogies:");
+        filtered.forEach(System.out::println);
+    }
+}
